f1e7d254e2eaadf67324aa146566852e344bd38e
Luis Paulo <luispaulosantos.br@gmail.com>
2026-07-19 17:16:47 -0300

    creates partials and adjusts layout.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -8,7 +8,7 @@ def index(request):
 
     print(contacts.query)
 
-    context = {"contacts": contacts}
+    context = {"contacts": contacts, "site_title": "Contatos - "}
 
     return render(request, "contact/index.html", context)
 
@@ -16,7 +16,8 @@ def index(request):
 def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    site_title = f"{single_contact.first_name} {single_contact.last_name} - "
 
-    context = {"contact": single_contact}
+    context = {"contact": single_contact, "site_title": site_title}
 
     return render(request, "contact/contact.html", context)
