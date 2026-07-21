7c7256235646b0b6d8472fd20160e90ef6c4bed2
Luis Paulo <luispaulosantos.br@gmail.com>
2026-07-21 12:07:57 -0300

    Updates forms.py and templates

--- contact/forms.py
+++ contact/forms.py
@@ -5,19 +5,48 @@ from contact.models import Contact
 
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={"class": "class-a class-b", "placeholder": "Escreva aqui"}
+        ),
+        label="Primeiro Nome",
+        help_text="Texto de ajuda",
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.fields["first_name"].widget.attrs.update(
+        #     {"class": "class-a class-b", "placeholder": "Escreva aqui..."}
+        # )
+
     class Meta:
         model = Contact
         fields = ("first_name", "last_name", "phone")
+        # widgets = {
+        #     "first_name": forms.TextInput(
+        #         attrs={"class": "class-a class-b", "placeholder": "Escreva aqui"}
+        #     )
+        # }
 
     def clean(self):
-        # cleaned_data = self.cleaned_data
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get("first_name")
+        last_name = cleaned_data.get("last_name")
 
-        self.add_error(
-            "first_name", ValidationError("Mensagem de erro", code="invalid")
-        )
-
-        self.add_error(
-            "first_name", ValidationError("Mensagem de erro 2", code="invalid")
-        )
+        if first_name == last_name:
+            msg = ValidationError(
+                "The first name cannot be same as the second name", code="invalid"
+            )
+            self.add_error("first_name", msg)
+            self.add_error("last_name", msg)
 
         return super().clean()
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get("first_name")
+
+        if first_name == "ABC":
+            self.add_error("first_name", ValidationError("Add_error", code="invalid"))
+
+        return first_name
